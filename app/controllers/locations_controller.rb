279e7a15e2b13dd6e578575492f55289b41ce878
Ccordova41 <ccordova41@gmail.com>
2017-12-29 23:07:54 -0500

    updated location controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,20 @@
 class LocationsController < ApplicationController
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def location_params
+      params.require(:location).permit(:name)
+    end
 end
