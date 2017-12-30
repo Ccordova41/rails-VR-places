0b5b506b879dea47666149c1201ed391fef86ebd
Ccordova41 <ccordova41@gmail.com>
2017-12-29 20:41:16 -0500

    updated seed date

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+#location
+location_1 = Location.create!(name: "New York City")
+location_2 = Location.create!(name: "Littleton, Colorado")
+
 #venues
 venue_1 = Venue.create!(name: "Jump into the Light", description: "Jump Into The Light is America’s first virtual reality cinema, play lab and studio. Located on the Lower East Side of Manhattan, it provides an array of virtual- and augmented-reality experiences: skydive in VR; climb a mountain; fly around the world in 360° video; arcade and gaming experiences.", location_id: location_1.id)
 
 #experiences
 experience_1 = Experience.create!(title: "VR Cinema", genre: "cinema", description: "Watch 360 Videos", venue_id: venue_1.id)
-
-#location
-location_1 = Location.create!(name: "New York City")
-location_2 = Location.create!(name: "Littleton, Colorado")
+experience_2 = Experience.create!(title: "Mountain Climbing", genre: "nature", description: "Climb heights without the danger", venue_id: venue_1.id)
 
 #categories
 action = Category.create!(name: "Action")
 adventure = Category.create!(name: "Adventure")
-travel = Travel.create!(name: "Travel")
+travel = Category.create!(name: "Travel")
 
 #experience_categories
-experience_categories = ExperienceCategory.create!(experience_id: experience.id, category_id: action.id)
+experience_categories = ExperienceCategory.create!(experience_id: experience_1.id, category_id: action.id)
 experience_categories_1 = ExperienceCategory.create!(experience_id: experience_1.id, category_id: adventure.id)
 experience_categories_2 = ExperienceCategory.create!(experience_id: experience_2.id, category_id: travel.id)
