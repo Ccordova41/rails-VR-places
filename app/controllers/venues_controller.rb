0c1d4e84140da98a00b6dc860cb71b2c04f382ba
Ccordova41 <ccordova41@gmail.com>
2018-01-02 22:13:00 -0500

    updated venues controller

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,43 @@
 class VenuesController < ApplicationController
 
-  def show
-    @venues = Venue.all
-  end
-end
+  before_action :venue_var, only: [:show, :edit, :update, :destroy, :collection]
+
+    def new
+      @venue = Venue.new
+    end
+
+    def create
+      @venue = Venue.new(venue_params)
+      if @venue.save
+        redirect_to venue_path(@venue)
+      else
+        render :new
+      end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+      if @venue.update(venue_params)
+        redirect_to venue_path(@venue)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @venue.destroy
+      redirect_to root_path
+    end
+
+    private
+      def venue_params
+        params.require(:venue).permit(:name, :description, :location_id, location_attributes: [:name])
+      end
+      def venue_var
+        @venue = Venue.find(params[:id])
+      endend
