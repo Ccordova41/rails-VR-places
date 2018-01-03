befa392aeeabde320174a468fcbd2898365b522c
Ccordova41 <ccordova41@gmail.com>
2018-01-02 22:09:15 -0500

    updated categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,33 @@
 class CategoriesController < ApplicationController
+  def index
+   @categories = Category.all
+ end
+
+ def new
+   @category = Category.new
+ end
+
+ def create
+   @category = Category.new(category_params)
+   if @category.save
+     redirect_to categories_path
+   else
+     render :new
+   end
+ end
+
+ def show
+   @category = Category.find(params[:id])
+ end
+
+ def destroy
+   @category = Category.find(params[:id])
+   @category.destroy
+   redirect_to categories_path
+ end
+
+ private
+   def category_params
+     params.require(:category).permit(:name)
+   end
 end
