d25c22152d00bc17059f49a8a48b520e2ec2efbb
Ccordova41 <ccordova41@gmail.com>
2018-06-19 23:38:02 -0400

    added typed.js

---
+++ app/assets/javascripts/typed.js
@@ -0,0 +1,10 @@
+// Can also be included with a regular script tag
+import Typed from 'typed.js';
+
+
+var typed = new Typed('.element', {
+  strings: ['My strings are: <i>strings</i> with', 'My strings are: <strong>HTML</strong>', 'My strings are: Chars &times; &copy;'],
+  typeSpeed: 0,
+  backSpeed: 0,
+  smartBackspace: true, // this is a default    loop: true
+});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  @array = ["Pizza","Sushi","Potato Chips"]
+
   def hello
     @venues = Venue.all
     respond_to do |format|
