a26f4399017e520f567de9fc61886f39600c8e05
Ccordova41 <ccordova41@gmail.com>
2018-06-20 21:31:27 -0400

    added typed

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,5 +2,5 @@ class Location < ApplicationRecord
   has_many :venues, dependent: :destroy;
 
   validates :name, presence: true
-  # validates :name, uniqueness: true
+  validates :name, uniqueness: true
 end
